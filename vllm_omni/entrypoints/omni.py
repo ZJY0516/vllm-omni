5bb68827f1c072aab9e2fe2d84c090956e5ec26e
zjy0516 <riverclouds.zhu@qq.com>
2026-01-24 15:05:19 +0800

    add __del__
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm_omni/entrypoints/omni.py
+++ vllm_omni/entrypoints/omni.py
@@ -470,6 +470,9 @@ class OmniBase:
     def is_async(self) -> bool:
         return False
 
+    def __del__(self) -> None:
+        self.close()
+
 
 class Omni(OmniBase):
     """Unified entrypoint for both LLM and Diffusion models for better usability.
@@ -570,7 +573,7 @@ class Omni(OmniBase):
                 return self._run_generation_with_generator(prompts, sampling_params_list)
             else:
                 outputs = list(self._run_generation(prompts, sampling_params_list))
-                self.close()
+                # self.close()
                 return outputs
         except Exception as e:
             logger.exception("[Orchestrator] Failed to run generation: %s", e)
